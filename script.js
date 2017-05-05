69f7729ae7bb81351e5bc31eddbe9940422d5c90
Becky Marshall <becky.marshall@id.iit.edu>
2017-05-04 20:58:28 -0500

    Finally got mailto: right!

--- script.js
+++ script.js
@@ -16,16 +16,15 @@ function updateUserInformation(index, user) {
     var $user = $('.user-profile').eq(index);
     $user.find("h3").text(user.name.first + " " + user.name.last);
     $user.find('.company').find('.name').text(user.company.name);
-    $user.find('.company').find('.street').text(user.company.street);
-    $user.find('.company').find('.city').text(user.company.city);
-    $user.find('.company').find('span').find('.state').text(user.company.state);
+    $user.find('.company').find('.street').text(user.company.address.street);
+    $user.find('.company').find('.city').text(user.company.address.city);
+    $user.find('.company').find('span').find('.state').text(user.company.address.state);
     $user.find('.company').find('.zip').text(user.company.zip);
     $user.find('a').text(user.company.email);
-    $user
-        .find('.email')
+    $user.find('.company')
         .find('a')
         .text(user.email)
-        .attr('href', "mailto:" + user.email);
+        .attr('href', "mailto:" + user.company.email);
 
 }
 
